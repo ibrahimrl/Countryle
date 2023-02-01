8f37293a2ed166b5760bec201d51e40e640dbd4d
Ibrahim Rahimli <ibrahim.rahimli20016@gmail.com>
2023-02-02 00:11:15 +0100

    Initial commit

---
+++ logic.py
@@ -0,0 +1,119 @@
+from json import load, dump
+from random import choice
+from typing import Any
+
+TOTAL_ATTEMPTS: int = 6
+
+class Country:
+    def __init__(self, name: str, info: dict[str, Any]):
+        self.Name: str = name
+        self.Continent: str = info['ContinentName']
+        self.Population: int = info['Population']
+        self.Temperature: int = info['Temperature']
+        self.Location: tuple = info['CapitalLatitude'], info['CapitalLongitude']
+
+    @property
+    def Hemisphere(self) -> str:
+        return 'NORTHERN' if self.Location[0] >= 0 else 'SOUTHERN'
+
+class Game:
+    def __init__(self):
+        self.Attempts: int = 6
+        self.GuessedCountries: set[str] = set()
+        self.DataGuessedCountries: list[tuple[Any]] = []
+        with open('Information.json') as file:
+            self._data: dict[str, Any] = load(file)
+        self.Target: Country = self._random_country()
+        with open('Results.json') as file:
+            self.FileLoad: dict[str, Any] = load(file)
+        self.FileLoad['NumberOfGames'] += 1
+
+    def _random_country(self) -> Country:
+        self.DataNames: list[str] = list(self._data.keys())
+        name: str = choice(self.DataNames)
+        return Country(name, self._data[name])
+
+    def new_country(self, name) -> Country | None:
+        if self.list_checker(name):
+            return self.entered_country_data(name)
+
+    def list_checker(self, name) -> bool:
+        if self._data.get(name):
+            return True
+        return False
+
+    def entered_country_data(self, name) -> Country | None:
+        if not self.already_guessed(name):
+            self.GuessedCountries.add(name)
+            return Country(name, self._data[name])
+
+    def already_guessed(self, name: str) -> bool:
+        return name in self.GuessedCountries
+
+    def _name(self, country: Country) -> str:
+        return f'{TOTAL_ATTEMPTS - self.Attempts}. {country.Name}'
+
+    def _hemisphere_check(self, first: Country, second: Country) -> tuple:
+        return first.Hemisphere, first.Hemisphere == second.Hemisphere
+
+    def _continent_check(self, first: Country, second: Country) -> tuple:
+        return first.Continent, first.Continent == second.Continent
+
+    def _population_check(self, first: Country, second: Country) -> tuple:
+        difference = first.Population - second.Population
+        if -500_000 < difference < 500_000:
+            return str(first.Population), ('True', difference)
+        if -1500_000 < difference < 1500_000:
+            return str(first.Population), ('Almost', difference)
+        return str(first.Population), ('False', difference)
+
+    def _temperature_check(self, first: Country, second: Country) -> tuple:
+        difference = first.Temperature - second.Temperature
+        temp = str(first.Temperature) + '°'
+        if -1 < difference < 1:
+            return temp, ('True', difference)
+        if -2 < difference < 2:
+            return temp, ('Almost', difference)
+        return temp, ('False', difference)
+
+    def _coord_diff(self, first: Country, second: Country) -> tuple:
+        return first.Location[0] - second.Location[0], first.Location[1] - second.Location[1]
+
+    def _direction(self, coordinates: tuple) -> str:
+        s: str = ''
+        s = s + 'S' if 1.5 < coordinates[0] else s if -1.5 <= coordinates[0] <= 1.5 else s + 'N'
+        s = s + 'W' if 1.5 < coordinates[1] else s if -1.5 <= coordinates[1] <= 1.5 else s + 'E'
+        if len(s) > 0:
+            return s, 'Coordinate'
+        return 'Target is found', 'True'
+
+    def win_results(self, country: Country):
+        self.FileLoad['Wins'] += 1
+        if country.Name not in self.FileLoad['Counties']:
+            self.FileLoad[country.Continent] += 1
+            self.FileLoad['Counties'].append(country.Name)
+
+    def check(self, first: Country, second: Country):
+        if first.Name != second.Name:
+            self.Attempts -= 1
+        data = self._name(first), self._hemisphere_check(first, second), self._continent_check(first, second), \
+               self._temperature_check(first, second), self._population_check(first, second), \
+               self._direction(self._coord_diff(first, second))
+        self.DataGuessedCountries.append(data)
+
+    def end_game(self) -> str:
+        with open('Results.json', 'w') as file:
+            dump(self.FileLoad, file)
+        return self.Target.Name
+
+class ResultsTable:
+    def __init__(self):
+        with open('Results.json') as file:
+            self.FileLoad = load(file)
+
+    def discovered(self) -> str:
+        return f'{round(len(self.FileLoad["Counties"]) / 185 * 100, 1):.1f}'
+
+    def continents(self, key: str) -> str:
+        TotalNumber = {'Asia': 42, 'Australia': 11, 'Africa': 48, 'Europe': 47, 'America': 37}
+        return f'{round(self.FileLoad[key] / TotalNumber[key] * 100, 1):.1f}'
---
+++ main.py
@@ -0,0 +1,160 @@
+from logic import *
+from tkinter import *
+from PIL import Image, ImageTk
+from tkintermapview import TkinterMapView
+
+CIRCLE_SIZE: int = 75
+HORIZONTAL_TEXT_MARGIN: int = 2
+VERTICAL_CIRCLE_MARGIN: int = 25
+VERTICAL_TEXT_MARGIN: int = 20
+Current_Point: tuple = (335, 190)
+
+COLOR: dict = {
+    'True': '#00903c',
+    'False': '#bb0823',
+    'Almost': '#f7b53e',
+    'Coordinate': '#57abdd',
+    'Submit': '#555555',
+    'Background': '#fafafa'}
+
+class View(Canvas):
+    def __init__(self, parent):
+        super().__init__(parent, width=1050, height=650, background=COLOR['Background'])
+        self.grid()
+        self.parent = parent
+        self.start_view()
+
+    def start_view(self):
+        self.text(525, 30, 'COUNTRYLE', 25, 'Black')
+
+        self.create_line(325, 65, 725, 65, width=1, fill='Black')
+        self.create_line(325, 540, 725, 540, width=1, fill='Black')
+        self.create_line(400, 602, 650, 602, width=1, fill='Black')
+
+        self.entry = Entry(self.parent, width=20, fg='Black', bg='#fafafa', bd=0, justify=CENTER)
+        self.entry.config(highlightbackground=COLOR['Background'], highlightcolor=COLOR['Background'])
+
+        self.create_window(525, 590, window=self.entry, anchor=CENTER)
+
+        self.image_placer()
+
+        self.create_rectangle(390, 560, 660, 620, width=1, outline='Black')
+        self.image_submit = image_resizer('img/submit.png')
+        self.button_submit = Button(self, image=self.image_submit, command=country_name, activebackground='red', bd=0)
+        self.button_submit.config(highlightbackground=COLOR['Background'], highlightcolor=COLOR['Background'])
+        self.create_window(700, 590, window=self.button_submit)
+
+    def image_placer(self):
+        self.image_map = PhotoImage(file='img/map.png')
+        self.create_image(525, 325, image=self.image_map, anchor=CENTER)
+
+        self.image_hem = image_resizer('img/hemisphere.png')
+        self.create_image(335, 110, image=self.image_hem, anchor=W)
+        self.text(360, 145, 'Hemisphere', 10, 'Black', justify=CENTER)
+
+        self.image_con = image_resizer('img/continent.png')
+        self.create_image(417, 110, image=self.image_con, anchor=W)
+        self.text(442, 145, 'Continent', 10, 'Black', justify=CENTER)
+
+        self.image_temp = image_resizer('img/avg-temperature.png')
+        self.create_image(525, 110, image=self.image_temp, anchor=CENTER)
+        self.text(525, 145, 'Avg.\nTemperature', 10, 'Black', justify=CENTER)
+
+        self.image_pop = image_resizer('img/population.png')
+        self.create_image(582, 110, image=self.image_pop, anchor=W)
+        self.text(607, 145, 'Population', 10, 'Black', justify=CENTER)
+
+        self.image_cord = image_resizer('img/direction.png')
+        self.create_image(715, 110, image=self.image_cord, anchor=E)
+        self.text(690, 145, 'Coordinates', 10, 'Black', justify=CENTER)
+
+        self.image_lower = image_resizer('img/lower.png', 10, 10)
+        self.image_higher = image_resizer('img/higher.png', 10, 10)
+        self.image_found = image_resizer('img/location.png', 30, 30)
+
+    def game_update(self, game_status):
+        global Current_Point
+        x, y = Current_Point
+        self.create_text(x - 10, y, text=game_status[0], anchor=W, font=('Helvetica', 20), fill='Black')
+        Current_Point = (x, y + VERTICAL_TEXT_MARGIN)
+        self.oval(game_status[1:])
+
+    def oval(self, game_status):
+        global Current_Point
+        x, y = Current_Point
+        for c, d in game_status:
+            if isinstance(d, tuple):
+                self.create_oval(x, y, x + CIRCLE_SIZE, y + CIRCLE_SIZE, width=0, fill=COLOR[str(d[0])])
+                if d[1] != 0:
+                    img, image_coordinate = self.higher_lower(d[1])
+                    self.create_image(x + CIRCLE_SIZE // 2, y + CIRCLE_SIZE // 2 + image_coordinate, image=img)
+            else:
+                self.create_oval(x, y, x + CIRCLE_SIZE, y + CIRCLE_SIZE, width=0, fill=COLOR[str(d)])
+            if c == 'Target is found':
+                self.create_image(x + CIRCLE_SIZE // 2, y + CIRCLE_SIZE // 2, image=self.image_found, anchor=CENTER)
+            else:
+                self.create_text(x + CIRCLE_SIZE // 2, y + CIRCLE_SIZE // 2, text=str(c), font=('Helvetica', 10), fill='White')
+            x += CIRCLE_SIZE + HORIZONTAL_TEXT_MARGIN
+        y += CIRCLE_SIZE + VERTICAL_CIRCLE_MARGIN
+        Current_Point = (335, y)
+
+    def text(self, x: int, y: int, text: str, size: int, color: str, **args):
+        self.create_text(x, y, text=text, font=('Helvetica', size), fill=color, **args)
+
+    def higher_lower(self, num) -> tuple:
+        return (self.image_higher, -20) if num < 0 else (self.image_lower, 20)
+
+    def redraw_board(self):
+        self.delete('all')
+        self.start_view()
+        global Current_Point
+        Current_Point = (335, 190)
+        n = len(NewGame.DataGuessedCountries)
+        i = 0 if n <= 3 else n - 3
+        for e in NewGame.DataGuessedCountries[i:]:
+            self.game_update(e)
+
+    def map(self, name):
+        root_tk = Tk()
+        root.title(name)
+        root_tk.geometry(f"{600}x{400}")
+        map_widget = TkinterMapView(root_tk, width=300, height=200, corner_radius=0)
+        map_widget.set_address(name, marker=True)
+
+def image_resizer(img, x=50, y=50):
+    given_img = (Image.open(img))
+    resized_img = given_img.resize((x, y), Image.Resampling.LANCZOS)
+    return ImageTk.PhotoImage(resized_img)
+
+def format_input(name):
+    return ' '.join(map(lambda x: x[0].upper() + x[1:].lower(), name.split(' ')))
+
+def country_name() -> str:
+    name: str = canvas.entry.get()
+    if len(name) > 0:
+        entered_country(format_input(name))
+
+
+NewGame: Game = Game()
+
+def entered_country(name):
+    CurrentCountry: Country = NewGame.new_country(name)
+    if CurrentCountry is not None:
+        canvas.entry.delete(0, END)
+        NewGame.check(CurrentCountry, NewGame.Target)
+        canvas.redraw_board()
+        if CurrentCountry.Name == NewGame.Target.Name:
+            NewGame.win_results(CurrentCountry)
+            NewGame.end_game()
+            # canvas.map(CurrentCountry)
+
+
+root: Tk = Tk()
+root.title('Countryle')
+canvas = View(root)
+root.mainloop()
+
+
+# f = ResultsTable()
+# print(f'Played games: {f.FileLoad["NumberOfGames"]}')
+# print(f'Wins: {f.FileLoad["Wins"]}')
